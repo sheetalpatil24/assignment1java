677ad6acf678c4bb47be009eefe9c5a7084ce268
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-06 01:34:35 +0530

    2nd question

--- Assignment1.java
+++ Assignment1.java
@@ -1,17 +1,25 @@
-public class EmpWageUC1 {
-
+public class EmpWageUC2 {
     public static void main(String[] args) {
-        
+        //constant
         int IS_FULL_TIME = 1;
-
-        double empCheck = Math.floor(Math.random() * 10) %2;
-        System.out.println(empCheck);
-        if (empCheck == IS_FULL_TIME)
+        int Emp_Rate_Per_Hour = 20;
+        //variables
+        int employeehr = 0;
+        int employeewage = 0;
+        //computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
         
+        if (empCheck == IS_FULL_TIME)
+        {
+        employeehr = 8;
+        }
+        else{
+        employeehr = 0;
+        }
 
-        System.out.println("Employee is Present");
-        else
-        System.out.println("Employee is Abscent");
-
-       }
+        employeewage = employeehr * Emp_Rate_Per_Hour;
+        System.out.println("Emp Wage: " + employeewage);
     }
+}
+    
+
