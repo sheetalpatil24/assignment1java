5199aaa5752c9c9ada048b48709bb03969090e65
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-06 00:14:58 +0530

    second commit

--- EmpWageUC1.java
+++ EmpWageUC1.java
@@ -5,7 +5,9 @@ public class EmpWageUC1 {
         int IS_FULL_TIME = 1;
 
         double empCheck = Math.floor(Math.random() * 10) %2;
+        System.out.println(empCheck);
         if (empCheck == IS_FULL_TIME)
+        
 
         System.out.println("Employee is Present");
         else
