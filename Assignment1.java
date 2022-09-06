f757ee6ae8fbd19693548e9d4d90a07da8d8d696
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-06 16:59:52 +0530

    sixth

--- Assignment1.java
+++ Assignment1.java
@@ -1,43 +1,52 @@
-public class wagesformonthUC5 { 
+public class Assignment1 {
 
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    
-    public static void main(String[] args) { 
-    
-        // Variables
-    
-        int empHrs = 0, empWage= 0, totalEmpWage = 0; 
-        // Computation
-         for (int day = 0; day< NUM_OF_WORKING_DAYS; day++) {
-             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-    
-             switch (empCheck) {
-    
-    
-    case IS_PART_TIME: 
-    empHrs = 4;
-    
-    break; 
-    case IS_FULL_TIME: 
-    empHrs = 8;
-    
-    break; 
-    default: empHrs = 0;
+public static final int IS_PART_TIME= 1;
+ public static final int IS_FULL_TIME = 2;
+ public static final int EMP_RATE_PER_HOUR=20; 
+ public static final int NUM_OF_WORKING_DAYS = 2; 
+ public static final int MAX_HRS_IN_MONTH = 18;
+
+ public static void main(String[] args) {
+
+
+// Variables 
+int empHrs = 0, totalEmpHrs = 0, totalworkingDays = 0;
+
+// Computation
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+ totalworkingDays < NUM_OF_WORKING_DAYS) {
+
+totalworkingDays++;
+
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+      case IS_PART_TIME:
+
+     empHrs = 4;
+
+   break;
+
+case IS_FULL_TIME: 
+empHrs = 8;
+
+break;
+
+default: 
+empHrs = 8;
+
+}
+
+totalEmpHrs += empHrs;
+
+System.out.println("Day#: "+ totalworkingDays + "Emp Hr: " +empHrs); 
 
  }
-    
-    empWage = empHrs * EMP_RATE_PER_HOUR ;
-     totalEmpWage += empWage; 
-     System.out.println("Emp Wage:" +empWage);
+ int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+
+System.out.println("Total Emp Wage: "+totalEmpwage);
+
+ }
 
 }
-    
-    System.out.println("Total Emp Wage: " + totalEmpWage);
-
-}
-    }
-    
-
