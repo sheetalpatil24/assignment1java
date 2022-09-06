659b747dfca8fcb48c61c1d047a3b9c3199cf838
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-06 23:49:20 +0530

    uc1

--- Assignment1.java
+++ Assignment1.java
@@ -1,29 +1,17 @@
-import java.util.Scanner;
-
-
 public class Assignment1 {
 
+  public static void main(String[] args) {
+      
+      int IS_FULL_TIME = 1;
 
-	
-	    public static void main( String[] args ) {
-	    	System.out.println("Welcome to Line Comparison Computation Program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the first co-ordinate");
-        int X1=sc.nextInt();
-        int Y1=sc.nextInt();
-        System.out.println("enter the second co-ordinate");
-        int X2=sc.nextInt();
-        int Y2=sc.nextInt();
-        System.out.println("the first co-ordinate is (" +X1 +","+Y1+") ");
-        System.out.println("the second co-ordinate is (" +X2 +","+Y2+") ");
-        double length2;
-        int point3=(X2-X1)*(X2-X1);
-        int point4=(Y2-Y1)*(Y2-Y1);
-        length2=Math.sqrt(point3+point4);
-        System.out.println("length of the line is "+length2);
-        
-      }
-    
+      double empCheck = Math.floor(Math.random() * 10) %2;
+      System.out.println(empCheck);
+      if (empCheck == IS_FULL_TIME)
+      
 
-    }
-	  
+      System.out.println("Employee is Present");
+      else
+      System.out.println("Employee is Abscent");
+
+     }
+  }
